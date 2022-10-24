e8f6725fb829c3e35feba924cf4eb91c5cfc7f52
Anna Uchaeva <anna280990@yahoo.com>
2022-10-24 18:04:08 +0300

    Add Yadisk.py

---
+++ YAdisk.py
@@ -0,0 +1,8 @@
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        # Функция может ничего не возвращать
